4cebfb3c1136c6fa0af9b09341b33c4c2cfefe37
ShevT <shev_t@mail.ru>
2013-10-23 16:33:55 +0400

    speed < 0

--- allpatches/traffic/Traffic.java
+++ allpatches/traffic/Traffic.java
@@ -117,6 +117,10 @@ public class Traffic extends TextView {
             @Override
             public void handleMessage(Message msg) {
                 speed = (getTotalReceivedBytes() - totalRxBytes) / 1024 / 3;
+                if (speed < 0) {
+                    speed = 0;
+                }
+
                 totalRxBytes = getTotalReceivedBytes();
 
                 DecimalFormat DecimalFormatfnum = new DecimalFormat("##0.00");
